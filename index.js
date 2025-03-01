5fbacbf4bd27ac3c277d9565e4f1f1be0ca01fcd
Petros Kouzoupis <peterkuzu@gmail.com>
2025-03-01 17:18:33 +0200

    First iteration

---
+++ index.js
@@ -0,0 +1,14 @@
+fetch("https://www.thecolorapi.com/scheme?hex=2b2b2b&mode=monochrome")
+    .then(res => res.json())
+    .then(data => console.log(data.colors))
+
+    /** Rough plan
+     * 1. event listener on form submit
+     * 2. get the values from input and select
+     * 3. fetch request with those values
+     * 4. render the colors 
+     * <div> //columns
+     *  <div></div> //color
+     *  <span></span> // hex
+     * </div>
+     */
